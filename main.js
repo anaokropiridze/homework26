2cbc36031f35adb61936768d7fc7d27fdc1cbf9a
anaokropiridze <ana.okro8@gmail.com>
2024-03-23 21:25:17 +0400

    26

---
+++ main.js
@@ -0,0 +1,30 @@
+// 3.1
+/* let int1 = 70;
+let int2 = 27;
+let result = (int1, int2) => (int1 / int2).toFixed(4);
+console.log(result(int1, int2)); */
+
+// 3.2
+/* let fahrenheitToCelsius = (celsius) => ((celsius - 32) * 5) / 9;
+console.log(fahrenheitToCelsius(90).toFixed(0)); */
+
+// 3.3
+/* let car1 = "bmw";
+let car2 = "bmw";
+let result;
+let include = (car1, car2) => {
+  if (car1 === car2) {
+    result = true;
+  } else {
+    result = false;
+  }
+};
+include(car1, car2);
+console.log(result); */
+
+// 3.4
+/* let myPow = (num1, num2) => Math.pow(num1, num2);
+console.log(myPow(3, 2)); */
+
+// 3.5;
+// let functionmultiplication = (x, y) => x * y;
